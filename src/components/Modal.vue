96b68c82a62174f5c8b5da69f4795354bae93854
Michael Joyner <joyner.michael@gmail.com>
2018-01-26 11:04:44 +0800

    release the transition css

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -80,25 +80,25 @@ export default {
   .modal-footer {
     max-height: 100px;
   }
-  .modal-enter,
-  .modal-leave {
-    opacity: 0;
-  }
-  .modal-enter .modal-container,
-  .modal-leave .modal-container {
-    -webkit-transform: scale(1.1);
-    transform: scale(1.1);
-  }
-  .modal-enter {
-    opacity: 0;
-  }
-  .modal-leave-active {
-    opacity: 0;
-  }
-  .modal-enter .modal-container,
-  .modal-leave-active .modal-container {
-    -webkit-transform: scale(1.1);
-    transform: scale(1.1);
-  }
+}
+.modal-enter,
+.modal-leave {
+  opacity: 0;
+}
+.modal-enter .modal-container,
+.modal-leave .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+.modal-enter {
+  opacity: 0;
+}
+.modal-leave-active {
+  opacity: 0;
+}
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
 }
 </style>
